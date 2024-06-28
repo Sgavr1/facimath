12052aa454a8276f6e630cb918e3376acf46e6d4
Oleksandr Havrylko <sanya.gavrilko60@gmail.com>
2024-06-28 10:31:12 +0300

    init

---
+++ src/main/java/com/facimath/facimath/FacimathApplication.java
@@ -0,0 +1,13 @@
+package com.facimath.facimath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacimathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacimathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facimath/facimath/FacimathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facimath.facimath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacimathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
