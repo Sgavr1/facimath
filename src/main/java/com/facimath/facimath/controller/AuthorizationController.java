af782aa4e186dbf31cf4399fb1cb79aaaf77aebd
ShkrSltn <shakir.sultanov@powercoders.org>
2024-07-01 11:17:57 +0200

    Have added CORS

--- src/main/java/com/facimath/facimath/controller/AuthorizationController.java
+++ src/main/java/com/facimath/facimath/controller/AuthorizationController.java
@@ -6,6 +6,7 @@ import com.facimath.facimath.service.TokenService;
 import com.facimath.facimath.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AuthorizationController {
     @Autowired
     private TokenService tokenService;
 
+    @CrossOrigin(origins = "http://localhost:4200/login")
     @GetMapping("/login")
     public ResponseEntity<JwtResponse> login(@RequestParam(name = "login") String login, @RequestParam(name = "password") String password) {
         try {
